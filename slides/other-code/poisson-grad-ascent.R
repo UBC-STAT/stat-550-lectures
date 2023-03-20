ff7127129bf2aa9346a190925b404e61f16a00e1
Daniel J. McDonald <dajmcdon@gmail.com>
2023-03-20 15:26:28 -0700

    revised unit testing

--- slides/other-code/poisson-grad-ascent.R
+++ slides/other-code/poisson-grad-ascent.R
@@ -1,27 +1,39 @@
-valid_par <- function(x) stopifnot(x > 0, is.finite(x))
-
-llike <- function(x) {
-  valid_par(x)
-  mean(dpois(y, x, TRUE))
+valid_par <- function(lam) {
+  stopifnot(is.numeric(lam), length(lam) == 1)
+  stopifnot(lam > 0, is.finite(lam))
 }
 
-dllike <- function(x) {
-  valid_par(x)
-  mean(y) / x - 1
+valid_data <- function(y) {
+  stopifnot(is.numeric(y))
+  stopifnot(all(y >= 0), all(is.finite(y)))
 }
 
-grad_ascent <- function(fn, gn, x0 = 1, gam = 1, maxit = 100, tol = 1e-6) {
-  f0 = fn(x0)
+llike <- function(lambda, y) {
+  valid_par(lambda)
+  valid_data(y)
+  
+  mean(y) * log(lambda) - lambda
+}
+
+dllike <- function(lambda, y) {
+  valid_par(lambda)
+  valid_data(y)
+  
+  mean(y) / lambda - 1
+}
+
+grad_ascent <- function(y, fn, gn, x0 = 1, gam = 1, maxit = 100, tol = 1e-6) {
+  f0 <- fn(x0, y)
   for (i in 1:maxit) {
-    x = x0 + gam * gn(x0)
-    f = fn(x0)
+    x <- x0 + gam * gn(x0, y)
+    f <- fn(x, y)
     if (abs(f - f0) < tol) break
-    f0 = f
-    x0 = x
+    f0 <- f
+    x0 <- x
   }
   if (i == maxit) warning("Did too many iterations. Try a different gamma.")
   x
 }
 
-y <- rpois(100, 3)
-grad_ascent(llike, dllike)
+yy <- rpois(100, 5)
+grad_ascent(yy, llike, dllike)
--- slides/other-code/poisson-tests.R
+++ slides/other-code/poisson-tests.R
@@ -1,16 +1,31 @@
 library(testthat)
+source("other-code/poisson-grad-ascent.R")
 
-y <- c(2,4,0,1)
+y <- c(2, 4, 0, 1) # shared by all tests
+
+test_that("validators work", {
+  expect_silent(valid_data(y))
+  expect_error(valid_data(c(1, -2, 0)))
+  expect_error(valid_data(Inf))
+  
+  expect_error(valid_par(y))
+  expect_silent(valid_par(1))
+  expect_error(valid_par(-1))
+  expect_error(valid_par(Inf))
+})
+
 
 test_that("log likelihood runs", {
-  y <- c(2,4,0,1)
-  expect_true(is.finite(llike(1)))
-  expect_error(llike(0))
-  expect_error(llike(-3))
+  expect_true(is.finite(llike(1, y)))
+  
+  # check our math
+  lam <- 2 # 1 is usually special
+  res <- mean(y) * log(lam) - lam
+  expect_equal(llike(lam, y), res)
+  expect_equal(
+    llike(lam, y), 
+    mean(dpois(y, lam, log = TRUE) + lgamma(y + 1))
+  )
 })
 
-y <- -1
-test_that("log likehood bombs on bad data", {
-  expect_equal(llike(3), -Inf)
-})
 
